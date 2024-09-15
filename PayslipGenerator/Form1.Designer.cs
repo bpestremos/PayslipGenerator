f5bb0728751ccf7d23fec4f616fc53383a0d711d
Brian Estremos <brianestremos28@gmail.com>
2024-09-15 17:55:21 +0800

    UI, computation and other updates

--- PayslipGenerator/Form1.Designer.cs
+++ PayslipGenerator/Form1.Designer.cs
@@ -32,7 +32,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.txtDaysIn = new System.Windows.Forms.TextBox();
             this.cmbEmp = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BtnGenerate = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtdailyRate = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -51,7 +51,8 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(73, 158);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(73, 169);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(126, 20);
             this.label2.TabIndex = 1;
@@ -60,7 +61,8 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(73, 192);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(73, 203);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(88, 20);
             this.label4.TabIndex = 3;
@@ -68,7 +70,7 @@
             // 
             // txtDaysIn
             // 
-            this.txtDaysIn.Location = new System.Drawing.Point(291, 255);
+            this.txtDaysIn.Location = new System.Drawing.Point(291, 266);
             this.txtDaysIn.Name = "txtDaysIn";
             this.txtDaysIn.Size = new System.Drawing.Size(214, 27);
             this.txtDaysIn.TabIndex = 4;
@@ -77,24 +79,25 @@
             // 
             this.cmbEmp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbEmp.FormattingEnabled = true;
-            this.cmbEmp.Location = new System.Drawing.Point(291, 155);
+            this.cmbEmp.Location = new System.Drawing.Point(291, 166);
             this.cmbEmp.Name = "cmbEmp";
             this.cmbEmp.Size = new System.Drawing.Size(214, 28);
             this.cmbEmp.TabIndex = 1;
             this.cmbEmp.SelectedIndexChanged += new System.EventHandler(this.cmbEmp_SelectedIndexChanged);
             // 
-            // button1
+            // BtnGenerate
             // 
-            this.button1.Location = new System.Drawing.Point(210, 407);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(169, 29);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Generate";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.BtnGenerate.Location = new System.Drawing.Point(210, 418);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.Size = new System.Drawing.Size(169, 29);
+            this.BtnGenerate.TabIndex = 8;
+            this.BtnGenerate.Text = "Generate";
+            this.BtnGenerate.UseVisualStyleBackColor = true;
+            this.BtnGenerate.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = global::PayslipGenerator.Properties.Resources.logo;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(187, 143);
@@ -104,15 +107,17 @@
             // 
             // txtdailyRate
             // 
-            this.txtdailyRate.Location = new System.Drawing.Point(291, 189);
+            this.txtdailyRate.Location = new System.Drawing.Point(291, 200);
             this.txtdailyRate.Name = "txtdailyRate";
+            this.txtdailyRate.ReadOnly = true;
             this.txtdailyRate.Size = new System.Drawing.Size(214, 27);
             this.txtdailyRate.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(73, 258);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(73, 269);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(107, 20);
             this.label1.TabIndex = 11;
@@ -120,7 +125,7 @@
             // 
             // txtAbsents
             // 
-            this.txtAbsents.Location = new System.Drawing.Point(291, 288);
+            this.txtAbsents.Location = new System.Drawing.Point(291, 299);
             this.txtAbsents.Name = "txtAbsents";
             this.txtAbsents.Size = new System.Drawing.Size(214, 27);
             this.txtAbsents.TabIndex = 5;
@@ -128,7 +133,8 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(73, 291);
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(73, 302);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(78, 20);
             this.label9.TabIndex = 13;
@@ -136,7 +142,7 @@
             // 
             // txtOthers
             // 
-            this.txtOthers.Location = new System.Drawing.Point(291, 354);
+            this.txtOthers.Location = new System.Drawing.Point(291, 365);
             this.txtOthers.Name = "txtOthers";
             this.txtOthers.Size = new System.Drawing.Size(214, 27);
             this.txtOthers.TabIndex = 7;
@@ -144,7 +150,8 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(73, 357);
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(73, 368);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(132, 20);
             this.label10.TabIndex = 15;
@@ -154,16 +161,17 @@
             // 
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(199, 46);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(210, 37);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(316, 51);
+            this.label3.Size = new System.Drawing.Size(363, 94);
             this.label3.TabIndex = 17;
-            this.label3.Text = "PAYSLIP GENERATOR";
+            this.label3.Text = "READSMART LEARNING CENTER";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // txtAdjustments
             // 
-            this.txtAdjustments.Location = new System.Drawing.Point(291, 321);
+            this.txtAdjustments.Location = new System.Drawing.Point(291, 332);
             this.txtAdjustments.Name = "txtAdjustments";
             this.txtAdjustments.Size = new System.Drawing.Size(214, 27);
             this.txtAdjustments.TabIndex = 6;
@@ -171,7 +179,8 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(73, 324);
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(73, 335);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(170, 20);
             this.label11.TabIndex = 18;
@@ -180,7 +189,8 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(73, 225);
+            this.label12.ForeColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(73, 236);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(141, 20);
             this.label12.TabIndex = 21;
@@ -188,7 +198,7 @@
             // 
             // txtWorkingDays
             // 
-            this.txtWorkingDays.Location = new System.Drawing.Point(291, 222);
+            this.txtWorkingDays.Location = new System.Drawing.Point(291, 233);
             this.txtWorkingDays.Name = "txtWorkingDays";
             this.txtWorkingDays.Size = new System.Drawing.Size(214, 27);
             this.txtWorkingDays.TabIndex = 3;
@@ -197,8 +207,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.SpringGreen;
-            this.ClientSize = new System.Drawing.Size(578, 469);
+            this.BackColor = System.Drawing.Color.SlateBlue;
+            this.ClientSize = new System.Drawing.Size(578, 489);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.txtWorkingDays);
             this.Controls.Add(this.txtAdjustments);
@@ -211,13 +221,15 @@
             this.Controls.Add(this.txtdailyRate);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnGenerate);
             this.Controls.Add(this.cmbEmp);
             this.Controls.Add(this.txtDaysIn);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Payslip Generator";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -236,7 +248,7 @@
         private Label label7;
         private Label label8;
         private TextBox textBox6;
-        private Button button1;
+        private Button BtnGenerate;
         private PrintPreviewControl printPreviewControl1;
         private PictureBox pictureBox1;
         private TextBox txtdailyRate;
--- PayslipGenerator/Form1.cs
+++ PayslipGenerator/Form1.cs
@@ -1,9 +1,13 @@
 using Microsoft.Extensions.Configuration;
 using PayslipGenerator.Classes;
+using PayslipGenerator.Properties;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 using System.Configuration;
 using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.PortableExecutable;
+using PdfSharp.UniversalAccessibility.Drawing;
 
 namespace PayslipGenerator
 {
@@ -16,8 +20,7 @@ namespace PayslipGenerator
         {
             InitializeComponent();
             BindEmployeesToComboBox();
-            pictureBox1.ImageLocation = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Images\logo.png");
-
+            //pictureBox1.ImageLocation = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Images\logo.png");
 
             txtdailyRate.KeyPress += new KeyPressEventHandler(txtNumeric_KeyPress);
             txtDaysIn.KeyPress += new KeyPressEventHandler(txtNumeric_KeyPress);
@@ -42,7 +45,7 @@ namespace PayslipGenerator
 
         private void txtNumeric_KeyPress(object sender, KeyPressEventArgs e)
         {
-            TextBox txt = sender as TextBox;
+            TextBox? txt = sender as TextBox;
 
             // Allow control characters like backspace and delete
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
@@ -51,7 +54,7 @@ namespace PayslipGenerator
             }
 
             // If a decimal point is entered, ensure only one exists in the textbox
-            if (e.KeyChar == '.' && txt.Text.Contains("."))
+            if (e.KeyChar == '.' && txt.Text.Contains('.'))
             {
                 e.Handled = true;
             }
@@ -73,7 +76,18 @@ namespace PayslipGenerator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //GeneratePayslip("test", "test position", 1250.0M, DateTime.Now.ToString("MMM/dd/yyyy"));
+            foreach (TextBox txt in this.Controls.OfType<TextBox>())
+            {
+                if (txt.Name != "txtdailyRate")
+                {
+                    if (txt.Text == string.Empty)
+                    {
+
+                    }
+
+                }
+            }
+
             GeneratePayslip();
         }
 
@@ -110,20 +124,20 @@ namespace PayslipGenerator
             XFont boldFont = new XFont("Verdana", 12, XFontStyleEx.Bold);
 
 
-            //// Page title fonts
-            XFont titleFont = new XFont("Lucida Console", 24, XFontStyleEx.Regular);
-            gfx.DrawString("CONFIDENTIAL PAYSLIP", titleFont, XBrushes.Black, new XRect(200, 100, page.Width, 30), XStringFormats.CenterLeft);
+            //// PAGE TITLE
+            XFont titleFont = new XFont("Verdana", 24, XFontStyleEx.Regular);
+            gfx.DrawString("CONFIDENTIAL PAYSLIP", titleFont, XBrushes.Black, new XRect(185, 80, page.Width, 30), XStringFormats.CenterLeft);
 
 
             // Load image (make sure the image path is correct)
-            string imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Images\logo.png");
-            if (File.Exists(imagePath))
+            //string imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Images\logo.png");
+            XImage xImage = ConvertResourceImageToXImage("logo");
+            if (xImage != null)
             {
-                XImage image = XImage.FromFile(imagePath);
 
                 // Calculate aspect ratio and fit the image proportionally within specified bounds
-                double imageWidth = image.PixelWidth;
-                double imageHeight = image.PixelHeight;
+                double imageWidth = xImage.PixelWidth;
+                double imageHeight = xImage.PixelHeight;
 
                 // Max dimensions for the image on the PDF
                 double maxImageWidth = 100;
@@ -137,7 +151,7 @@ namespace PayslipGenerator
                 double scaledHeight = imageHeight * scale;
 
                 // Draw image at the top-left corner with scaled size
-                gfx.DrawImage(image, 50, 30, scaledWidth, scaledHeight);
+                gfx.DrawImage(xImage, 50, 30, scaledWidth, scaledHeight);
             }
             else
             {
@@ -157,32 +171,32 @@ namespace PayslipGenerator
 
 
             // Draw table headers
-            DrawCell(gfx, tableX, tableY, columnWidth1, rowHeight, "Period", boldFont);
-            DrawCell(gfx, tableX + columnWidth1, tableY, columnWidth2, rowHeight, "Gross", boldFont);
-            DrawCell(gfx, tableX + columnWidth1 + columnWidth2, tableY, columnWidth3, rowHeight, "Deductions", boldFont);
-            DrawCell(gfx, tableX + columnWidth1 + columnWidth2 + columnWidth3, tableY, columnWidth4, rowHeight, "Net", boldFont);
+            DrawCell(gfx, tableX, tableY, columnWidth1, rowHeight, "Period", boldFont, false, true);
+            DrawCell(gfx, tableX + columnWidth1, tableY, columnWidth2, rowHeight, "Gross", boldFont, false, true);
+            DrawCell(gfx, tableX + columnWidth1 + columnWidth2, tableY, columnWidth3, rowHeight, "Deductions", boldFont, false, true);
+            DrawCell(gfx, tableX + columnWidth1 + columnWidth2 + columnWidth3, tableY, columnWidth4, rowHeight, "Net", boldFont, false, true);
 
             // Draw table rows
             double currentY = tableY + rowHeight;
-            DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "Basic Salary", font);
+            DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "   Basic Salary", font);
             DrawCell(gfx, tableX + columnWidth1, currentY, columnWidth2, rowHeight, particulars.BasicSalary.ToString("N2"), font, true);
             DrawCell(gfx, tableX + columnWidth1 + columnWidth2, currentY, columnWidth2, rowHeight, string.Empty, font, true);
             DrawCell(gfx, tableX + columnWidth1 + columnWidth2 + columnWidth3, currentY, columnWidth2, rowHeight, string.Empty, font, true);
 
             currentY += rowHeight;
-            DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "Add: Adjustments", font);
+            DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "   Add: Adjustments", font);
             DrawCell(gfx, tableX + columnWidth1, currentY, columnWidth2, rowHeight, particulars.Adjustments.ToString("N2"), font, true);
             DrawCell(gfx, tableX + columnWidth1 + columnWidth2, currentY, columnWidth2, rowHeight, string.Empty, font, true);
             DrawCell(gfx, tableX + columnWidth1 + columnWidth2 + columnWidth3, currentY, columnWidth2, rowHeight, string.Empty, font, true);
 
             currentY += rowHeight;
-            DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "Less: Absences", font);
+            DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "   Less: Absences", font);
             DrawCell(gfx, tableX + columnWidth1, currentY, columnWidth2, rowHeight, string.Empty, font, true);
             DrawCell(gfx, tableX + columnWidth1 + columnWidth2, currentY, columnWidth2, rowHeight, particulars.Absents.ToString("N2"), font, true);
             DrawCell(gfx, tableX + columnWidth1 + columnWidth2 + columnWidth3, currentY, columnWidth2, rowHeight, string.Empty, font, true);
 
             currentY += rowHeight;
-            DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "Others", font);
+            DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "   Others", font);
             DrawCell(gfx, tableX + columnWidth1, currentY, columnWidth2, rowHeight, string.Empty, font, true);
             DrawCell(gfx, tableX + columnWidth1 + columnWidth2, currentY, columnWidth2, rowHeight, particulars.Others.ToString("N2"), font, true);
             DrawCell(gfx, tableX + columnWidth1 + columnWidth2 + columnWidth3, currentY, columnWidth2, rowHeight, string.Empty, font, true);
@@ -190,36 +204,46 @@ namespace PayslipGenerator
             if (showGovBenefits)
             {
                 currentY += rowHeight;
-                DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "Government Benefits", font, true);
+                DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "   Government Benefits", font, true);
                 DrawCell(gfx, tableX + columnWidth1, currentY, columnWidth2, rowHeight, string.Empty, font);
                 DrawCell(gfx, tableX + columnWidth1 + columnWidth2, currentY, columnWidth2, rowHeight, string.Empty, font);
                 DrawCell(gfx, tableX + columnWidth1 + columnWidth2 + columnWidth3, currentY, columnWidth2, rowHeight, string.Empty, font);
 
                 currentY += rowHeight;
-                DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "SSS", font, true);
+                DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "   SSS", font, true);
                 DrawCell(gfx, tableX + columnWidth1, currentY, columnWidth2, rowHeight, string.Empty, font);
                 DrawCell(gfx, tableX + columnWidth1 + columnWidth2, currentY, columnWidth2, rowHeight, "<t3>", font);
                 DrawCell(gfx, tableX + columnWidth1 + columnWidth2 + columnWidth3, currentY, columnWidth2, rowHeight, string.Empty, font);
 
                 currentY += rowHeight;
-                DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "PHIC", font, true);
+                DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "   PHIC", font, true);
                 DrawCell(gfx, tableX + columnWidth1, currentY, columnWidth2, rowHeight, string.Empty, font);
                 DrawCell(gfx, tableX + columnWidth1 + columnWidth2, currentY, columnWidth2, rowHeight, "<t3>", font);
                 DrawCell(gfx, tableX + columnWidth1 + columnWidth2 + columnWidth3, currentY, columnWidth2, rowHeight, string.Empty, font);
 
                 currentY += rowHeight;
-                DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "HDMF", font, true);
+                DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "   HDMF", font, true);
                 DrawCell(gfx, tableX + columnWidth1, currentY, columnWidth2, rowHeight, string.Empty, font);
                 DrawCell(gfx, tableX + columnWidth1 + columnWidth2, currentY, columnWidth2, rowHeight, "<t3>", font);
                 DrawCell(gfx, tableX + columnWidth1 + columnWidth2 + columnWidth3, currentY, columnWidth2, rowHeight, string.Empty, font);
             }
 
             currentY += rowHeight;
-            DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "Total", boldFont, true);
+            DrawCell(gfx, tableX, currentY, columnWidth1, rowHeight, "Total    ", boldFont, true);
             DrawCell(gfx, tableX + columnWidth1, currentY, columnWidth2, rowHeight, particulars.TotalGross.ToString("N2"), font, true);
-            DrawCell(gfx, tableX + columnWidth1 + columnWidth2, currentY, columnWidth3, rowHeight, particulars.TotalDeduction.ToString("N2"), font);
+            DrawCell(gfx, tableX + columnWidth1 + columnWidth2, currentY, columnWidth3, rowHeight, particulars.TotalDeduction.ToString("N2"), font, true);
             DrawCell(gfx, tableX + columnWidth1 + columnWidth2 + columnWidth4, currentY, columnWidth3, rowHeight, particulars.TotalNet.ToString("N2"), font, true);
 
+
+
+
+            //// SIGNATORY
+            XFont signatory = new XFont("Verdana", 12, XFontStyleEx.Regular);
+            XPen pen = new XPen(XColors.Black, 2);
+            gfx.DrawString("Certified by : ", signatory, XBrushes.Black, new XRect(50, 400, page.Width, 30), XStringFormats.CenterLeft);
+            gfx.DrawLine(pen, 50, 470, 250, 470);
+            gfx.DrawString(config["AppSettings:Signatory"].ToString().ToUpper(), signatory, XBrushes.Black, new XRect(50, 480, page.Width, 30), XStringFormats.CenterLeft);
+            gfx.DrawString(config["AppSettings:Position"].ToString().ToUpper(), signatory, XBrushes.Black, new XRect(50, 500, page.Width, 30), XStringFormats.CenterLeft);
 
             // Save the document
             document.Save(filePath);
@@ -231,15 +255,14 @@ namespace PayslipGenerator
             });
         }
 
-        static void DrawCell(XGraphics gfx, double x, double y, double width, double height, string text, XFont font, bool isRight = false)
+        static void DrawCell(XGraphics gfx, double x, double y, double width, double height, string text, XFont font, bool isRight = false, bool isHeaders = false)
         {
             // Draw the cell border
             gfx.DrawRectangle(XPens.Black, x, y, width, height);
 
             // Draw the text inside the cell
-            gfx.DrawString(text, font, XBrushes.Black, new XRect(x, y, width, height), (isRight ? XStringFormats.CenterRight : XStringFormats.CenterLeft));
+            gfx.DrawString(text, font, XBrushes.Black, new XRect(x, y, width, height), (isRight ? XStringFormats.CenterRight : (isHeaders ? XStringFormats.Center : XStringFormats.CenterLeft)));
         }
-
 
         public Payroll GetPayroll()
         {
@@ -280,6 +303,28 @@ namespace PayslipGenerator
                     txt.Text = string.Empty;
 
                 txt.TextAlign = HorizontalAlignment.Right;
+            }
+        }
+
+
+        public static XImage ConvertResourceImageToXImage(string imageName)
+        {
+            // Load image from resources
+            Bitmap bitmap = Resources.ResourceManager.GetObject(imageName) as Bitmap;
+
+            if (bitmap == null)
+            {
+                throw new ArgumentException("Image not found in resources.");
+            }
+
+            // Convert Bitmap to MemoryStream
+            using (MemoryStream stream = new MemoryStream())
+            {
+                bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                stream.Position = 0; // Reset stream position
+
+                // Create XImage from MemoryStream
+                return XImage.FromStream(stream);
             }
         }
     }
